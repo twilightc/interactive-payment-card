f3ef213e40244fa6dc2202cdbe08d80171bd3e3c
tychang <timmyc2007@gmail.com>
2024-02-15 10:35:54 +0800

    card-rotate effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,35 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const isCardFlipped = isFlipped ? 'card-flipped' : '';
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-[600px] h-[400px]" style={{ perspective: '800px' }}>
+        <div
+          className={`relative w-full h-full card ${isCardFlipped}`}
+          style={{ transformStyle: 'preserve-3d' }}
+          onClick={() => {
+            setIsFlipped(!isFlipped);
+          }}
+        >
+          <div className="card-common card-front" style={{ background: '#ff0000' }}>
+            front
+          </div>
+          <div
+            className="card-common card-back"
+            style={{
+              background: '#0000ff',
+              transform: 'rotateY(180deg)'
+            }}
+          >
+            back
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
 }
